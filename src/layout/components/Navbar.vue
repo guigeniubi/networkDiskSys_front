1cab2119b9f8e80f5d6371ae8b3d5bc5b38052f7
guigeniubi <1042684036@qq.com>
2024-04-07 10:10:37 +0800

    fileShare

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,7 +7,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar + '?imageView2/1/w/80/h/80'" class="user-avatar">
+          <img :src= "require('@/assets/kneel.png')" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
@@ -125,8 +125,8 @@ export default {
 
         .user-avatar {
           cursor: pointer;
-          width: 40px;
-          height: 40px;
+          width: 50px;
+          height: 50px;
           border-radius: 10px;
         }
 
